545206d8e9e69b646afe8bf5cf116a785e42dde8
Elena Sharova <elena.sharova@gmail.com>
2022-07-09 18:18:48 +0100

    Added main and formatted

--- pandas/monte_carlo.py
+++ pandas/monte_carlo.py
@@ -1,17 +1,11 @@
 # xlslim examples of Monte Carlo Simulations
 
 import pandas as pd
-from numpy import  exp, sqrt, mean
+from numpy import exp, sqrt, mean
 from numpy.random import default_rng
 
-<<<<<<< Updated upstream
 
-def monte_carlo_stock_price(
-    s0: float, r: float, sigma: float, mat: float, strike: float = 0.0, N=10000
-) -> pd.DataFrame:
-=======
-def monte_carlo_stock_price(s0:float, r:float, sigma:float, mat:float, strike:float=0.0, N=10000)->pd.DataFrame:
->>>>>>> Stashed changes
+def monte_carlo_stock_price(s0: float, r: float, sigma: float, mat: float, strike: float = 0.0, N=10000) -> pd.DataFrame:
     """
         s0:     current stock price
         r:      interest rate for discounting
@@ -20,23 +14,29 @@ def monte_carlo_stock_price(s0:float, r:float, sigma:float, mat:float, strike:fl
         strike: optional strike to calculate call option payout
         N:      optional number of simulations
     """
-    assert mat>0, "Maturity should be positive"
-    
+    assert mat > 0, "Maturity should be positive"
+
     rng = default_rng(seed=11)
-    
+
     z = rng.standard_normal(size=N)
-    df_stock = pd.DataFrame(data = {'sim_stock': s0*exp((r-0.5*sigma*sigma)*mat + sigma*sqrt(mat)*z)})
-    
-    
-    if strike !=0.0:
-        df_stock['payout'] = df_stock.apply(lambda x: exp(-r*mat)*max((x.sim_stock-strike), 0.0), axis=1)
-        
+    df_stock = pd.DataFrame(
+        data={'sim_stock': s0*exp((r-0.5*sigma*sigma)*mat + sigma*sqrt(mat)*z)})
+
+    if strike != 0.0:
+        df_stock['payout'] = df_stock.apply(
+            lambda x: exp(-r*mat)*max((x.sim_stock-strike), 0.0), axis=1)
+
     return df_stock
-        
-    
-def average_payout(df: pd.DataFrame)->float:
-    
+
+
+def average_payout(df: pd.DataFrame) -> float:
+
     assert 'payout' in df.columns, "Passed Dataframe should have payout column"
-    
-    return mean(df['payout']) 
-    
+
+    return mean(df['payout'])
+
+
+if __name__ == "__main__":
+
+    mc = monte_carlo_stock_price(100.0, 0.02, 0.15, 2.0, 98.0)
+    print(f"Call option price {average_payout(mc)}")
