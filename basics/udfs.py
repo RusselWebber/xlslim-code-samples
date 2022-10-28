95be2657e3d67c7e9f362b0f753b0c3ed90fd030
Russel Webber <24542073+RusselWebber@users.noreply.github.com>
2022-10-28 14:49:08 +0100

    Correct typo in magic global name

--- basics/udfs.py
+++ basics/udfs.py
@@ -3,14 +3,19 @@
 # See https://support.microsoft.com/en-us/office/euroconvert-function-79c8fd67-c665-450c-bb6c-15fc92f8345c
 
 # This global is used to store a function pointer set by xlSlim
-XLSIM_UDFFUNC = None
+XLSLIM_UDFFUNC = None
 
 
 def udf_circle_area(radius: float) -> float:
     """Calculate the area of a circle using the VBA function AreaCircle"""
-    return XLSIM_UDFFUNC("AreaCircle", radius)
+    return XLSLIM_UDFFUNC("AreaCircle", radius)
 
 
 def eur_conversion(amount: float, from_currency: str, to_currency: str) -> float:
     """Convert to EUR using the Excel addin function EUROCONVERT."""
-    return XLSIM_UDFFUNC("EUROCONVERT", amount, from_currency, to_currency)
+    return XLSLIM_UDFFUNC(
+        "EUROCONVERT",
+        amount,
+        from_currency,
+        to_currency,
+    )
